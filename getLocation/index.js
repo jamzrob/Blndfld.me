449e44080213678f50e0709c97c915f4552e9ea7
Your Email Adress <raubenheimerjames@gmail.com>
2017-10-21 15:44:28 -0500

    User Location
    Finds user location

--- getLocation/index.js
+++ getLocation/index.js
@@ -3,6 +3,7 @@ function geoFindMe() {
 
   if (!navigator.geolocation){
     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
   }
 
   function success(position) {
