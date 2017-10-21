535113054b3bea410da3480d57cd84cf3cea3b9e
Joey Holliday <joeyholliday77@gmail.com>
2017-10-21 15:10:50 -0500

    Add some methods. Please check my work

--- public/uber.js
+++ public/uber.js
@@ -1,3 +1,25 @@
+// Redirect user to login page
+function loginWithUber(){
+    let loginURL = "https://login.uber.com/oauth/v2/authorize?client_id=JYapbqi3iPPitJR6PHifGN0To2qhsPUi&response_type=code";
+    window.location.href = loginURL;
+}
+
+// Get locally stored token
+function getToken(){
+    return localStorage.uberToken;
+}
+
+// Get token from Uber from auth_code
+function fetchToken(auth_code){
+    return fetch("http://127.0.0.1/getToken/"+code).then(response => response.text()).then(
+        text =>{
+            localStorage.uberToken = JSON.parse(text).access_token;
+            return localStorage.uberToken;
+        }
+    );
+}
+
+// Convience method ignore
 function getProductID(products, product){
     for(p in products){
         if(p.display_name===product){
@@ -7,6 +29,7 @@ function getProductID(products, product){
     return null;
 }
 
+// Get list of availble products and their ids
 function getProducts(token, long, lat){
     fetch("http://127.0.0.1/products"+token+"/"+lat+"/"+long).then(response => response.text()).then(
         text => {
@@ -18,4 +41,35 @@ function getProducts(token, long, lat){
             }
         }
     )
+}
+
+// Get user data
+function getUserData(){
+    return fetch("http://127.0.0.1/me/"+getToken()).then(response => response.text()).then(
+        text => {
+            return JSON.parse(text);
+        }
+    );
+}
+
+// Get ride estimate
+function getRideEstimate(start_lat, start_long, end_lat, end_long){
+    return fetch("http://127.0.0.1/request/estimate/"+getToken()+"/"
+                +start_lat+"/"+start_long+"/"
+                +end_lat+"/"+end_long).then(response => response.text()).then(
+        text => {
+            return JSON.parse(text);
+        }
+    );
+}
+
+// Schedule Ride
+function scheduleRide(fare_id, start_lat, start_long, end_lat, end_long){
+    return fetch("http://127.0.0.1/request/"+getToken()+"/"
+                +fare_id+"/"+start_lat+"/"+start_long+"/"
+                +end_lat+"/"+end_long).then(response => response.text()).then(
+        text => {
+            return JSON.parse(text);
+        }
+    );
 }
